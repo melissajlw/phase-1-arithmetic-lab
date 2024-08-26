72a2027d441c7ead8d0f505c932d694529a963dd
Melissa Wang <melissa.wang21@gmail.com>
2024-08-26 12:54:45 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// multiply is an equation whose multiplied results will equal 62
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+// random generates a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+
+// mod is an equation that calculates a remainder that is equal to 4
+let num3 = 29;
+let num4 = 5;
+let mod = 29 - (Math.floor(num3/num4) * num4);
+
+// max will return 20 as the highest number in the set
+let max = Math.max(1, 4, 7, 10, 20)
